611e9ad5e2d997576ef62bcd5368a1e8ada78d3d
JaeHyeon-KAIST <alvinlog@kaist.ac.kr>
2024-12-04 07:42:44 +0900

    StatusBar 검정색으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Router from './router/Router.tsx';
+import {StatusBar} from 'react-native';
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
       <SafeAreaProvider>
+        <StatusBar barStyle="dark-content" backgroundColor="#F4FDED" />
         <Router />
       </SafeAreaProvider>
     </NavigationContainer>
