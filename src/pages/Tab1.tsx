454ddd2d4dcf79220ed0f42d3ca5157e8f2287fe
JaeHyeon-KAIST <alvinlog@kaist.ac.kr>
2024-12-09 23:20:19 +0900

    FCM 토큰 클립보드 복사 기능

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,7 +1,22 @@
 import {Text} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Clipboard from '@react-native-clipboard/clipboard';
+import {useEffect} from 'react';
+import requestUserPermission from '../utils/requestUserPermission.tsx';
 
 export default function Tab1() {
+  useEffect(() => {
+    const fetchFCMToken = async () => {
+      const token = await requestUserPermission();
+      if (token) {
+        Clipboard.setString(token);
+        console.log('FCM Token:', token);
+      }
+    };
+
+    fetchFCMToken();
+  }, []);
+
   return (
     <SafeAreaView
       style={{
